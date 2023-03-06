dc8c80c3d797a2665f3055c87230984fcdb4b482
rutikeyone <rutikeyone@gmail.com>
2023-03-06 11:15:28 +0700

    Add localication and modified details

--- app/src/main/java/com/newsapp/feature/details/Details.kt
+++ app/src/main/java/com/newsapp/feature/details/Details.kt
@@ -1,19 +1,37 @@
 package com.newsapp.feature.details
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import com.newsapp.R
+import androidx.appcompat.app.AppCompatActivity
 import com.newsapp.core.constants.BundleConstants
+import com.newsapp.databinding.ActivityDetailsBinding
 import com.newsapp.feature.news.domain.entity.NewsEntity
+import com.squareup.picasso.Picasso
+
 
 class Details : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsBinding
+    private lateinit var picasso: Picasso
     private lateinit var newsEntity: NewsEntity
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
 
-        newsEntity = this.intent.getParcelableExtra<NewsEntity>(BundleConstants.NEWS)!!
-        Log.i("MYLOG", newsEntity.toString())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        picasso = Picasso.get()
+        newsEntity = this.intent.getParcelableExtra(BundleConstants.NEWS)!!
+        picasso.load(newsEntity.urlToImage).into(binding.idIVDetails)
+
+        binding.idTVHeadingDetails.text = newsEntity.title
+        binding.idTVDescriptionDetails.text = newsEntity.content
+        binding.idBtnReadFullNews.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(newsEntity.url))
+            startActivity(intent)
+        }
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
     }
 }
